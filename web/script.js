1a9ffdadd240d88808b696ed02379f03f5a13a7e
ICavlek <ivan.cavlek1403@gmail.com>
2024-12-18 17:23:00 +0100

    feat: added init wasm pack build

---
+++ web/script.js
@@ -0,0 +1,12 @@
+import init, { add } from './pkg/web.js';
+
+async function run() {
+  await init();
+  const result = add(1, 2);
+  console.log(`1 + 2 = ${result}`);
+  if (result !== 3) {
+    throw new Error("wasm addition doesn't work!");
+  }
+}
+
+run();
