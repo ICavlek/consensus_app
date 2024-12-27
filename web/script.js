b8065590dad33cc1f54bad1c08f7f47d58fb4be1
ICavlek <ivan.cavlek1403@gmail.com>
2024-12-27 13:08:07 +0100

    feat: implemented verification in wasm, fails

--- web/script.js
+++ web/script.js
@@ -1,33 +1,51 @@
-import init, { add } from './pkg/web.js';
+import init, { verify } from './pkg/web.js';
 
-async function run() {
-  await init();
-  const result = add(1, 2);
-  console.log(`1 + 2 = ${result}`);
-  if (result !== 3) {
-    throw new Error("wasm addition doesn't work!");
-  }
-}
+var trusted_signed_header;
+var peer_id;
+var validator;
 
 function connect_socket() {
   const ws = new WebSocket("ws://127.0.0.1:26657/websocket");
   const message = '{"jsonrpc": "2.0", "method": "subscribe", "id": 0, "params": {"query": "tm.event=\'NewBlock\'"}}';
   ws.onopen = (event) => {
     ws.send(message);
+    trusted_signed_header = get_latest_signed_header();
+    peer_id = get_peer_id();
+    validator = get_validator();
   };
   ws.onmessage = (event) => {
-    console.log(event.data);
+    let untrusted_signed_header = get_latest_signed_header();
+    let res = verify(untrusted_signed_header, trusted_signed_header, peer_id, validator);
+    console.log(res);
   }
 }
 
-function send_status() {
+function get_peer_id() {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "http://127.0.0.1:26657/status", false);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send();
-  console.log(xhr.responseText);
+  let response = JSON.parse(xhr.responseText);
+  return response.result.node_info.id;
 }
 
-run();
+function get_validator() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://127.0.0.1:26657/validators", false);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send();
+  let response = JSON.parse(xhr.responseText);
+  return response.result.validators[0];
+}
+
+function get_latest_signed_header() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://127.0.0.1:26657/commit", false);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send();
+  let response = JSON.parse(xhr.responseText);
+  return response.result.signed_header;
+}
+
+await init();
 connect_socket();
-send_status();
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,24 +1,56 @@
+use std::time::Duration;
+use tendermint::{time::Time, trust_threshold::TrustThresholdFraction};
+use tendermint_light_client_verifier::{
+    options::Options,
+    types::{LightBlock, ValidatorSet},
+    ProdVerifier, Verdict, Verifier,
+};
 use wasm_bindgen::prelude::*;
 
-// Called when the Wasm module is instantiated
-#[wasm_bindgen(start)]
-fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
-    Ok(())
-}
-
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+pub fn verify(
+    untrusted_signed_header: JsValue,
+    trusted_signed_header: JsValue,
+    peer_id: JsValue,
+    validator: JsValue,
+) -> JsValue {
+    let untrusted_signed_header = serde_wasm_bindgen::from_value(untrusted_signed_header).unwrap();
+    let trusted_signed_header = serde_wasm_bindgen::from_value(trusted_signed_header).unwrap();
+    let peer_id = serde_wasm_bindgen::from_value(peer_id).unwrap();
+    let validator = serde_wasm_bindgen::from_value(validator).unwrap();
+
+    let validators = ValidatorSet::without_proposer(vec![validator]);
+    let trusted_light_block = LightBlock::new(
+        trusted_signed_header,
+        validators.clone(),
+        validators.clone(),
+        peer_id,
+    );
+
+    let untrusted_light_block = LightBlock::new(
+        untrusted_signed_header,
+        validators.clone(),
+        validators.clone(),
+        peer_id,
+    );
+
+    let verifier = ProdVerifier::default();
+    let options = Options {
+        trust_threshold: TrustThresholdFraction::new(1, 3).unwrap(),
+        trusting_period: Duration::new(1209600, 0),
+        clock_drift: Duration::new(5, 0),
+    };
+    web_sys::console::log_1(&serde_wasm_bindgen::to_value(&untrusted_light_block).unwrap());
+    web_sys::console::log_1(&serde_wasm_bindgen::to_value(&trusted_light_block).unwrap());
+    web_sys::console::log_1(&serde_wasm_bindgen::to_value(&options).unwrap());
+    let result = verifier.verify_update_header(
+        untrusted_light_block.as_untrusted_state(),
+        trusted_light_block.as_trusted_state(),
+        &options,
+        Time::now(),
+    );
+    match result {
+        Verdict::Success => return JsValue::from_str("SUCCESS"),
+        _ => return JsValue::from_str("FAILED"),
+    }
 }
